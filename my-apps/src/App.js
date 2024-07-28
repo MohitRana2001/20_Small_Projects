2ce12cbe57f5073bd1ee320aa67fc1a5751db953
MOHIT RANA <awesomemohit20@gmail.com>
2024-07-28 11:47:57 +0530

    added image slider for rendering

--- my-apps/src/App.js
+++ my-apps/src/App.js
@@ -2,13 +2,18 @@ import './App.css';
 import Accordion from './components/Accordion';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
-
+import ImageSlider from './components/image-slider';
 function App() {
   return (
     <div className="App">
       <Accordion />
       <RandomColor />
       <StarRating />
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </div>
   )
 }
