ded31c046fee8bba0729da4ac37b5c5957c945aa
MOHIT RANA <awesomemohit20@gmail.com>
2024-07-28 11:03:55 +0530

    Added star-rating for rendering

--- my-apps/src/App.js
+++ my-apps/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import Accordion from './components/Accordion';
 import RandomColor from './components/random-color';
+import StarRating from './components/star-rating';
 
 function App() {
   return (
     <div className="App">
-      {/* <Accordion /> */}
+      <Accordion />
       <RandomColor />
+      <StarRating />
     </div>
-  );
+  )
 }
 
 export default App;
