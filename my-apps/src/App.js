6cae2ce758efd2634bfd36cc78b109b5c073d15a
MOHIT RANA <awesomemohit20@gmail.com>
2024-07-29 18:48:03 +0530

    rendered load-more-data

--- my-apps/src/App.js
+++ my-apps/src/App.js
@@ -3,6 +3,7 @@ import Accordion from './components/Accordion';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
 import ImageSlider from './components/image-slider';
+import LoadMoreData from './components/load-more-data';
 function App() {
   return (
     <div className="App">
@@ -14,6 +15,7 @@ function App() {
         page={"1"}
         limit={"10"}
       />
+      <LoadMoreData />
     </div>
   )
 }
