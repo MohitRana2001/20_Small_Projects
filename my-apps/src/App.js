9dd3cf132ba6313316ba1129a5e3a1e006c600b2
MOHIT RANA <awesomemohit20@gmail.com>
2024-07-30 10:07:32 +0530

    rendered qr-code and tree-view

--- my-apps/src/App.js
+++ my-apps/src/App.js
@@ -4,10 +4,13 @@ import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
 import ImageSlider from './components/image-slider';
 import LoadMoreData from './components/load-more-data';
+import TreeView from './components/tree-view';
+import menus from "./components/tree-view/data";
+import QRCodeGenrator from './components/qr-code-generator';
 function App() {
   return (
     <div className="App">
-      <Accordion />
+      {/* <Accordion />
       <RandomColor />
       <StarRating />
       <ImageSlider
@@ -15,7 +18,9 @@ function App() {
         page={"1"}
         limit={"10"}
       />
-      <LoadMoreData />
+      <LoadMoreData /> */}
+      {/* <TreeView menus={menus}/> */}
+      <QRCodeGenrator />
     </div>
   )
 }
